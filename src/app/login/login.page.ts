9c1a6458c8886f0375a9c4ccb44aa3b54c5079b0
expmaestro <expmaestro@gmail.com>
2020-04-20 21:10:45 +0300

    added login module

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  public loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
