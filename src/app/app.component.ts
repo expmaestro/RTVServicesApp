a737e90b4d646ec35f4851d88f0b0e3f800b89aa
expmaestro <expmaestro@gmail.com>
2020-05-11 14:20:44 +0300

    network

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,45 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Network } from '@ionic-native/network/ngx';
+import { BaseComponent } from './services/base-component';
+import { NetworkService } from './services/network.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent extends BaseComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private network: Network,
+    private networkService: NetworkService
   ) {
+    super();
     this.initializeApp();
+    this.network.onConnect().safeSubscribe(this, s => setTimeout(() => {
+      this.networkService.setState = true;
+      console.log('Network connected: ' + this.network.type);
+      console.log(navigator.onLine);
+    }, 0));
+
+    this.network.onDisconnect().safeSubscribe(this, s => setTimeout(() => {
+      this.networkService.setState = false;
+      console.log('Network disconnected: ' + this.network.type) 
+      console.log(navigator.onLine);
+    }, 0));
+    
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.styleLightContent();
+      // this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
