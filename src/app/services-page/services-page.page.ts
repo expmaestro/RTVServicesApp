ed4c1493dd8327d31615a712d02dad6e5f186f5f
expmaestro <expmaestro@gmail.com>
2020-05-18 13:29:09 +0300

    service page

--- src/app/services-page/services-page.page.ts
+++ src/app/services-page/services-page.page.ts
@@ -2,21 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { ServiceModel, DataService } from '../services/data.service';
+import { SettingsService, Profile } from '../services/settings.service';
+import { BaseComponent } from '../services/base-component';
 
 @Component({
   selector: 'app-services-page',
   templateUrl: './services-page.page.html',
   styleUrls: ['./services-page.page.scss'],
 })
-export class ServicesPagePage implements OnInit {
+export class ServicesPagePage extends BaseComponent implements OnInit {
   current = {};
   services: Array<ServiceModel>;
-  constructor(private nav: NavController, private router: Router, private dataService: DataService) { }
+  public data: Profile;
+
+  constructor(private nav: NavController, private router: Router, private dataService: DataService, private settingsService: SettingsService) { 
+    super();
+     this.settingsService.getProfileDataWithRequest();
+   }
 
   ngOnInit() {
-    console.log('Component Init')
-    this.services = this.dataService.getServices;
-    
+    this.services = this.dataService.getServices;   
+    this.settingsService.getProfileData.safeSubscribe(this, r => {
+      this.data = r;
+    });
   }
 
   next(service: ServiceModel) {
