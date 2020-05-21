60e905d9c26042ce69c5bf33262a017c69d6b999
expmaestro <expmaestro@gmail.com>
2020-05-21 12:58:44 +0300

    auth guard service

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,22 +1,44 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { NavController } from '@ionic/angular';
+import { SettingsService } from './settings.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor() { }
-  canActivate(route: import("@angular/router").ActivatedRouteSnapshot,
-    state: import("@angular/router").RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+  constructor(private nav: NavController, private settingsService: SettingsService) { }
+  canActivate(route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot) {
     return Observable.create(observer => {
-      let userAuthenticated = true; // check a token value
-      if(userAuthenticated) {
-
+      let url: string = state.url;
+      if (url.endsWith("login")) {
+        console.log('endWith login')
+        observer.next(true);
+        observer.complete();
+      } else {
+        this.settingsService
+          .IsLoggedIn()
+          .then(r => {
+            observer.next(r);
+            observer.complete();
+            if (!r) {
+              this.navigateToLogin();
+            }
+          })
+          .catch(r => {
+            this.navigateToLogin();
+            observer.next(false);
+            observer.complete();
+          });
       }
-      observer.next(userAuthenticated);
-      observer.complete();
     });
+  }
+
+  navigateToLogin() {
+    console.log('Login')
+    this.nav.navigateRoot("/login");
   }
 }
