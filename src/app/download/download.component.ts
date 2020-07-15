c76c5c6288b8c9cdf3a41e5c5719c889adaaa64d
expmaestro <expmaestro@gmail.com>
2020-07-15 13:39:38 +0300

    cancel download files

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, NgZone } from '@angular/core';
+import { Component, OnInit, Input, NgZone, OnDestroy } from '@angular/core';
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
 import { Platform, AlertController } from '@ionic/angular';
 import { FilesService } from '../services/files.service';
@@ -14,7 +14,7 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './download.component.html',
   styleUrls: ['./download.component.scss'],
 })
-export class DownloadComponent extends BaseComponent implements OnInit {
+export class DownloadComponent extends BaseComponent implements OnInit, OnDestroy {
   playlist: PlayListModel[] = [];
   @Input() set setPlaylist(value: PlayListModel[]) {
     this.playlist = value;
@@ -30,6 +30,8 @@ export class DownloadComponent extends BaseComponent implements OnInit {
   progress = 0;
   size = 0;
   alreadyLoaded: boolean = undefined;
+  isAndroid = false;
+  isIos = false;
   constructor(private fileTransfer: FileTransfer,
     private platform: Platform, private fileService: FilesService, private file: File,
     private alertController: AlertController,
@@ -41,12 +43,29 @@ export class DownloadComponent extends BaseComponent implements OnInit {
 
   ngOnInit() {
     this.platform.ready().then(() => {
+      if (this.platform.is("android")) { this.isAndroid = true; }
+      if (this.platform.is("ios")) { this.isIos = true; }
       if (this.platform.is("android") || this.platform.is("ios")) {
         this.fileTransferCreate = this.fileTransfer.create();
+
         this.fileTransferCreate.onProgress(progress => {
           if (this.playlist && this.playlist.length === 1) {
             this.zone.run(() => {
-              this.progress = Math.round(progress.loaded / progress.total * 100);
+              // console.log(progress);
+              if (progress.lengthComputable) {
+                // this.getFreeSpace().then(freeDiskSpace => {
+                //   console.log(`${progress.total} - ${freeDiskSpace}`)
+                //   if (freeDiskSpace > 0 && progress.total > (freeDiskSpace - 500 * 1024 * 1024)) {
+                //     freeDiskSpace = 0;
+                //     this.fileTransferCreate.abort();
+                //     this.downloadFileError(`Недостаточно места для скачивания файла`, false);
+                //   }
+                // });
+                this.progress = Math.round(progress.loaded / progress.total * 100);
+              }
+              else {
+                //console.log(progress.loaded / 1024 / 1024);
+              }
             });
           }
         });
@@ -54,9 +73,12 @@ export class DownloadComponent extends BaseComponent implements OnInit {
     });
   }
 
-
+  ngOnDestroy() {
+    this.cancelDownload();
+  }
 
   isPlaylistDownloaded() {
+    // platform ?
     this.fileService.getFileListFromFolder(this.fileService.getAudioFolder + `/${this.serviceId}`).then((f) => {
       let files: Entry[] = f;
       this.playlist.forEach(f => {
@@ -71,13 +93,32 @@ export class DownloadComponent extends BaseComponent implements OnInit {
         .then((s) => (this.size = s / 1024 / 1024))
         .catch(() => this.size = 0);
     }).catch(() => this.size = 0);
-  }
+  };
 
   deleteMusic() {
     console.log('delete ' + this.serviceId);
-    this.fileService
-      .clear(this.serviceId)
-      .then(t => this.isPlaylistDownloaded());
+    this.clear(true);
+  }
+
+  cancelDownload() {
+    if (this.needToDownloadFiles.length > 0) {
+      console.log('cancel, destroy');
+      this.fileTransferCreate.abort();
+      this.clear();
+    }
+  }
+
+
+  private getFreeSpace() {
+    return this.file.getFreeDiskSpace().then(result => {
+      if (this.isAndroid) {
+        return (result * 1024);
+      }
+      if (this.isIos) {
+        return result;
+      }
+      return 0;
+    });
   }
 
   private setDescription() {
@@ -100,6 +141,7 @@ export class DownloadComponent extends BaseComponent implements OnInit {
   }
 
   async download() {
+
     this.fileService
       .clear(this.serviceId, false)
       .then(() => this.downloadPlayList());
@@ -109,6 +151,7 @@ export class DownloadComponent extends BaseComponent implements OnInit {
     if (!playList) {
       this.progress = 0;
     }
+
 
     // var networkState = navigator.connection.type;
     // console.log(networkState);
@@ -140,6 +183,14 @@ export class DownloadComponent extends BaseComponent implements OnInit {
                 throw (error);
               })
         } catch (error) {
+          if (error.code === 4) { //when user canceled download
+            console.log(' user canceled download');
+            //  this.needToDownloadFiles = [];
+            throw error;
+          }
+          if (error.code === 3 && error.exception && error.exception.includes('ENOSPC')) {
+            throw error;
+          }
           console.log(i);
           const isLastAttempt = i + 1 === retryCount;
           if (isLastAttempt) {
@@ -153,24 +204,25 @@ export class DownloadComponent extends BaseComponent implements OnInit {
     const downloadPlayLsit = async () => {
       while (this.needToDownloadFiles.length > 0) {
         const currentDownloaded = this.needToDownloadFiles[0];
-        const fullUrl = environment.cdn + currentDownloaded.path;
+        const fullUrl = (currentDownloaded.path.includes('https') ? '' : environment.cdn) + currentDownloaded.path;
         const filePath = this.fileService.getFullFilePath(this.serviceId, this.fileService.getFileNameFromSrc(currentDownloaded.path));
 
         return await download_retry(currentDownloaded, fullUrl, filePath, 3)
           .then(async (t) => {
+            // console.log(7777777777777777777)
             this.needToDownloadFiles.shift();
             await downloadPlayLsit();
           })
           .catch((e) => {
-            console.log('popup');
             let textError = '';
             switch (e.code) {
               case 1: textError = 'Файл не найден на сервере'; break; // FileTransferError.FILE_NOT_FOUND_ERR Сервер не доступен либо файл не найден на сервере
               case 2: textError = 'INVALID_URL_ERR'; break; // FileTransferError.INVALID_URL_ERR
-              case 3:
+              case 3:  // FileTransferError.CONNECTION_ERR
 
                 if (e.exception && e.exception.includes('ENOSPC')) {
                   textError = 'Недостаточно места';
+                  throw 'ENOSPC';
                 } else
                   if (e.exception && e.exception.includes('Unable to resolve')) {
                     textError = 'Нет соединения с сервером';
@@ -179,14 +231,23 @@ export class DownloadComponent extends BaseComponent implements OnInit {
                     textError = 'Сервер разорвал соединение';
                   }
 
-                break; // FileTransferError.CONNECTION_ERR
-              case 4: textError = 'ABORT_ERR'; break; // FileTransferError.ABORT_ERR
+                break;
+              case 4: // FileTransferError.ABORT_ERR
+                throw 'ABORT_ERR';
               case 5: textError = 'NOT_MODIFIED_ERR'; break;  // FileTransferError.NOT_MODIFIED_ERR
             }
             this.downloadFileError(`Не удается скачать файл: <b>${currentDownloaded.name}</b> <br> Причина: <b>${textError}</b>`);
           })
+          .catch(e => {
+            if (e === 'ABORT_ERR') {
+              this.needToDownloadFiles = [];
+              console.log('User canceled download');
+            }
+            if (e === 'ENOSPC') {
+              this.downloadFileError(`Не удается скачать файл: <b>${currentDownloaded.name}</b> <br> Причина: <b>Недостаточно места</b>`, false);
+            }
+          })
           .finally(() => {
-            //this.loading.dismiss()
           })
       }
     }
@@ -200,35 +261,46 @@ export class DownloadComponent extends BaseComponent implements OnInit {
   }
 
 
-  private async downloadFileError(message: string) {
+  clear(showLoader = false) {
+    this.fileService
+      .clear(this.serviceId, showLoader).then(() => {
+        this.needToDownloadFiles = [];
+        this.fileService.updateFiles(this.serviceId);
+        this.isPlaylistDownloaded();
+      })
+  }
+
+  private async downloadFileError(message: string, showRetry = true) {
+
+    let buttons = [];
+
+    buttons.push({
+      text: 'Отменить',
+      role: 'cancel',
+      cssClass: 'secondary',
+      handler: () => {
+        console.log('Confirm Cancel');
+        this.clear();
+      }
+    })
+
+    if (showRetry) {
+      buttons.push({
+        text: 'Повторить',
+        handler: () => {
+          console.log('Confirm Ok');
+          this.downloadPlayList(this.needToDownloadFiles);
+        }
+      })
+    }
+
     const alert = await this.alertController.create({
       header: 'Ошибка',
-      //subHeader: message,
       message: message,
-      buttons: [
-        {
-          text: 'Отменить',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: () => {
-            console.log('Confirm Cancel');
-            this.needToDownloadFiles = [];
-          }
-        }, {
-          text: 'Повторить',
-          handler: () => {
-            console.log('Confirm Ok');
-            this.downloadPlayList(this.needToDownloadFiles);
-          }
-        }
-      ]
+      buttons: buttons
     });
 
     await alert.present();
   }
-
-  // getFolderSize(serviceId) {
-  //   this.fileService.directorySize(this.serviceId).then();
-  // }
 
 }
